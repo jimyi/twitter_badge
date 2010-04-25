a18e04c37888061232bf05194250295723845cc4
Jim Yi <jim@jimyi.com>
2010-04-25 18:29:47 -0400

    update author

--- twitter_badge.php
+++ twitter_badge.php
@@ -24,95 +24,89 @@
 	
 	class Twitter_Badge {
 
-		var $url = 'http://twitter.com/statuses/user_timeline.rss?screen_name=';
+		private $url = 'http://twitter.com/statuses/user_timeline.rss?screen_name=';
 
-		var $tweets = array();
-		var $last_update;
+		private $tweets = array();
+		private $last_update;
 
-		public function __construct($config_array)
-		{
+		public function __construct($config_array) {
 			// load the config values into the object
-			foreach ($config_array as $key => $value)
-			{
+			foreach ($config_array as $key => $value) {
 				$this->$key = $value;
 			}
 
 			$this->load_tweets();
 		}
 
-		public function display_tweets()
-		{
+		public function display_tweets() {
 			// print the tweets as list items
 			foreach ($this->tweets as $update) {
-				$item  = "<li>{$update['status']}&nbsp;<a href=\"{$update['link']}\">";
-				$item .= $this->calculate_age($update['created'])."</a></li>\n";
+				$status = htmlentities($update['status']);
+				// parse URLs into links
+				$status = ereg_replace("[[:alpha:]]+://[^ ]+","<a href=\"\\0\">\\0</a>", $status);
+				// parse any @users into links
+				$status = ereg_replace("@([a-zA-Z0-9_]+)","<a href=\"http://twitter.com/\\1\">\\0</a>", $status);
+
+				$item  = '<li>' . $status . '&nbsp;';
+				$item .= '<a href="' . htmlentities($update['link']) . '">';
+				$item .= $this->calculate_age($update['created']) . "</a></li>\n";
 				echo $item;
 			}
 		}
 
 		/** begin private functions **/
 
-		private function load_tweets()
-		{
+		private function load_tweets() {
 			$this->load_file_cache();
 
 			// retrieve tweets from Twitter if the last update is too old
-			// count is checked in case there was an error
+			// the return of fetch_tweets is checked in case there was an error
 			// loading the feed (API limit, twitter down, etc)
-			if ($this->is_cache_old() && $this->fetch_tweets() != 0)
-			{
+			if ($this->is_cache_old() && $this->fetch_tweets() != 0) {
 				$this->save_file_cache();
 			}
 		}
 
-		private function fetch_tweets()
-		{
+		private function fetch_tweets() {
 			$doc = new DOMDocument('1.0', 'UTF-8');
-			@$doc->load($this->url.$this->username); // fetch the user's RSS feed
+			$fetch = @$doc->load($this->url . $this->username); // fetch the user's RSS feed
 
 			$count = 0;
 
-			$this->tweets = array();
-			foreach ($doc->getElementsByTagName('item') as $node) {
-				if ($count == $this->update_count) break;
+			if ($fetch) {
+				$this->tweets = array();
+				foreach ($doc->getElementsByTagName('item') as $node) {
+					if ($count == $this->update_count) break;
 
-				$status = $node->getElementsByTagName('title')->item(0)->nodeValue;
-				// each status starts with "username: " - filter that out
-				$status = substr($status, strpos($status, ':') + 2);
-				if ($status[0] == '@' && $this->hide_replies) continue;
+					$status = $node->getElementsByTagName('title')->item(0)->nodeValue;
+					// each status starts with "username: " - filter that out
+					$status = substr($status, strpos($status, ':') + 2);
+					// filter out replies
+					if ($status[0] == '@' && $this->hide_replies) continue;
 
-				$status = htmlspecialchars($status);
-				// parse URLs into links
-				$status = ereg_replace("[[:alpha:]]+://[^ ]+","<a href=\"\\0\">\\0</a>", $status);
-				// parse any @users into links
-				$status = ereg_replace("@([a-zA-Z0-9_]+)","<a href=\"http://twitter.com/\\1\">\\0</a>", $status);
+					$link = $node->getElementsByTagName('link')->item(0)->nodeValue;
 
-				$link = $node->getElementsByTagName('link')->item(0)->nodeValue;
-				// url encode quotes just to be safe; don't want this to break our html in any way
-				$link = str_replace('"', '%22', $link);
-
-				$update = array (
-					'status' => $status,
-					'link' => $link,
-					'created' => $node->getElementsByTagName('pubDate')->item(0)->nodeValue
+					$update = array (
+						'status' => $status,
+						'link' => $link,
+						'created' => $node->getElementsByTagName('pubDate')->item(0)->nodeValue
 					);
-				array_push($this->tweets, $update);
-				$count++;
+					array_push($this->tweets, $update);
+					$count++;
+				}
 			}
 			return $count;
 		}
 
-		private function is_cache_old()
-		{
+		private function is_cache_old() {
 			$time_since = time() - $this->last_update;
 			return $time_since >= $this->update_time * 60;
 		}
 
-		private function load_file_cache()
-		{
+		private function load_file_cache() {
 			$data = @file_get_contents($this->filename);
-			if (!$data) // the cache file might not exist yet
-			{
+			if (!$data) {
+				// the cache file might not exist yet
 				return;
 			}
 			$this->tweets = unserialize($data);
@@ -120,18 +114,14 @@
 			unset($this->tweets[$this->update_count]);
 		}
 
-		private function save_file_cache()
-		{
+		private function save_file_cache() {
 			// make the last item our update time
 			$this->tweets[$this->update_count] = time();
-
 			file_put_contents($this->filename, serialize($this->tweets));
-
 			unset($this->tweets[$this->update_count]);
 		}
 
-		private function calculate_age($date)
-		{
+		private function calculate_age($date) {
 			$age = time() - strtotime($date);
 
 			// twitter style string for age
@@ -144,5 +134,5 @@
 			// we don't do X months ago, even if days is > 30
 			// besides, if one your most recent tweets is over a month ago, maybe you should tweet more :)
 		}
+
 	}
-?>
